a95f7914e2f752e78ef1c16b60b291ff1f656cca
Trang Le <trang.thule@zoho.com>
2025-08-11 02:41:30 +0700

    change port number for api server

--- packages/api-server/app.js
+++ packages/api-server/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 require('./redis-client');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Import route modules
 const indexRoutes = require('./routes/index');
